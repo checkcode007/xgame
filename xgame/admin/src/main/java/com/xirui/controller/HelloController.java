6a2b407410009dc6f5a6b48ee49095709fdc33ae
checkcode007 <zcj007w@163.com>
2023-07-13 19:02:47 +0800

    2

---
+++ xgame/admin/src/main/java/com/xirui/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.xirui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//@Controller
+//@RequestMapping("/hello")
+public class HelloController {
+//    @RequestMapping("/string")
+    public String returnString() {
+        return "string";
+    }
+}
--- xgame/net/src/main/java/com/xirui/Main.java
+++ xgame/net/src/main/java/com/xirui/Main.java
@@ -1,11 +1,7 @@
 package com.xirui;
 
-import java.util.concurrent.TimeUnit;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-
     }
 }
